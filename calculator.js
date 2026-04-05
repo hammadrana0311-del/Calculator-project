6069a3a3271b23b93ef6122905c1035a2a76f5df
hammadrana0311-del <hammad.rana0311@gmail.com>
2026-04-05 04:23:56 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
+function append(value) {
+  document.getElementById("display").value += value;
+}
 
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function backspace() {
+  let display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  let display = document.getElementById("display");
+
+  try {
+    display.value = eval(display.value);
+  } 
+  catch (error) {
+    display.value = "Error";
+  }
+}
